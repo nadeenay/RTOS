50ead646a5d34e2751a4c1841b454f6a17f60883
nadeenay <70846138+nadeenay@users.noreply.github.com>
2021-09-17 22:22:27 +0200

    small Edit

--- RTOS/Examples/Example1.c
+++ RTOS/Examples/Example1.c
@@ -22,21 +22,21 @@ void First_Task(void *pvParameters)
 {
     while(1)
     {    /*Turn the red led*/
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x02);
+        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1, GPIO_PIN_1);
 
 
         /*Delay */
         SysCtlDelay(2000000);
 
         /*Turn the blue*/
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_2);
+        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_2, GPIO_PIN_2);
 
         /*Delay */
         SysCtlDelay(2000000);
 
         /*Turn the green */
 
-        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1, 0x08);
+        GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3, GPIO_PIN_3);
 
         /*Delay */
         SysCtlDelay(2000000);
